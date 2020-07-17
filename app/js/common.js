8bbbd70bef9f2045cc3df85048a0da2ca1ebebcd
Vitatiy <targetdeveloper57@gmail.com>
2020-07-17 18:56:42 +0300

    left menu styling

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,23 @@
 $(function () {
 	var sectionsSwiper = new Swiper('.swiper-container', {
+		effect: 'fade',
 		fadeEffect: { crossFade: true },
-		autoHeight: true,
 		direction: 'vertical',
 		slidesPerView: 1,
 		allowTouchMove: false,
 		mousewheel: {
 			eventsTarget: '.wrapper-content'
 		},
-		effect: 'fade',
 		pagination: {
 			el: '.nav-inner',
 			clickable: true,
 			renderBullet: function (index, className) {
 				let nameSlide = this.slides[index].dataset.name,
-					outterMenu = $('.nav-outter > .nav-outter__list');
+					outterMenu = $('.nav-outter > .nav-outter__list'),
+					indexAttr = index >= 9 ? index+1 : '0'+parseInt(index+1);
 
-					outterMenu.append(`<li class="nav-outter__item" data-index="${index}">${nameSlide}</li>`);
-				return '<span class="' + className + '">' + nameSlide + '</span>';
+				outterMenu.append(`<li class="nav-outter__item" data-index="${index}">${nameSlide}</li>`);
+				return '<span class="' + className + '" data-index="'+indexAttr+'">' + nameSlide + '</span>';
 			},
 		},
 		on: {
@@ -41,21 +41,21 @@ $(function () {
 		},
 	});
 	//navigation outter change section
-	$('.nav-outter .nav-outter__item').click(function() {
+	$('.nav-outter .nav-outter__item').click(function () {
 		let itemMenu = $(this),
 			index = itemMenu.data('index'),
 			wrapper = $('.wrapper'),
 			allItemMenu = $('.nav-outter > .nav-outter__list .nav-outter__item'),
 			hamburger = $('.hamburger');
 
-			sectionsSwiper.slideTo(index);
-			allItemMenu.removeClass('nav-outter__item--active');
-			itemMenu.addClass('nav-outter__item--active');
-			wrapper.removeClass('wrapper--open');
-			hamburger.removeClass('is-active');
+		sectionsSwiper.slideTo(index);
+		allItemMenu.removeClass('nav-outter__item--active');
+		itemMenu.addClass('nav-outter__item--active');
+		wrapper.removeClass('wrapper--open');
+		hamburger.removeClass('is-active');
 	});
 	//open outter menu
-	$('.hamburger').click(function(){
+	$('.hamburger').click(function () {
 		$('.wrapper').toggleClass('wrapper--open');
 		$(this).toggleClass('is-active');
 	});
@@ -64,8 +64,19 @@ $(function () {
 		let currentIndex = sectionsSwiper.activeIndex,
 			itemsOutterMenu = $('.nav-outter .nav-outter__item');
 
-			itemsOutterMenu.removeClass('nav-outter__item--active');
-			itemsOutterMenu.parent().find(`[data-index=${currentIndex}]`).addClass('nav-outter__item--active');
+		itemsOutterMenu.removeClass('nav-outter__item--active');
+		itemsOutterMenu.parent().find(`[data-index=${currentIndex}]`).addClass('nav-outter__item--active');
+	});
+
+	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+	let vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	window.addEventListener('resize', () => {
+		// We execute the same script as before
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
 	});
 
 });
