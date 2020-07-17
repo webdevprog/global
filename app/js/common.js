4bb6296ae92ce6d224c3243ed06f4347212138dc
Vitatiy <targetdeveloper57@gmail.com>
2020-07-17 14:48:41 +0300

    swipe section, mousemove

--- app/js/common.js
+++ app/js/common.js
@@ -2,13 +2,38 @@ $(function () {
 	var swiper = new Swiper('.swiper-container', {
 		fadeEffect: { crossFade: true },
 		direction: 'vertical',
+		loop: false,
 		slidesPerView: 1,
-		spaceBetween: 30,
-		mousewheel: true,
+		spaceBetween: 0,
+		slidesPerColumnFill: 'row',
+		allowTouchMove: false,
+		mousewheel: {
+			eventsTarget: '.wrapper'
+		},
+		followFinger: true,
 		effect: 'fade',
 		pagination: {
-			el: '.swiper-pagination',
+			el: '.nav-inner',
 			clickable: true,
+		},
+		on: {
+			init: function () {
+				let _self = this;
+
+				var ts;
+				$('.wrapper').bind('touchstart', function (e) {
+					ts = e.originalEvent.touches[0].clientY;
+				});
+
+				$('.wrapper').bind('touchend', function (e) {
+					var te = e.originalEvent.changedTouches[0].clientY;
+					if (ts > te) {
+						_self.slideNext();
+					} else if (ts < te) {
+						_self.slidePrev();
+					}
+				});
+			},
 		},
 	});
 });
