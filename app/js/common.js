31b94ac59627963b7215b1e7ad3c3219e868cb32
Vitatiy <targetdeveloper57@gmail.com>
2020-07-17 16:53:06 +0300

    added func change active item menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,8 @@
 $(function () {
-	var swiper = new Swiper('.swiper-container', {
+	var sectionsSwiper = new Swiper('.swiper-container', {
 		fadeEffect: { crossFade: true },
+		autoHeight: true,
+		calculateHeight: false,
 		direction: 'vertical',
 		loop: false,
 		slidesPerView: 1,
@@ -19,14 +21,14 @@ $(function () {
 				let nameSlide = this.slides[index].dataset.name,
 					outterMenu = $('.nav-outter > .nav-outter__list');
 
-					outterMenu.append(`<li class="nav-outter__item">${nameSlide}</li>`);
+					outterMenu.append(`<li class="nav-outter__item" data-index="${index}">${nameSlide}</li>`);
 				return '<span class="' + className + '">' + nameSlide + '</span>';
 			},
 		},
 		on: {
 			init: function () {
 				let _self = this;
-				console.log(this)
+
 				var ts;
 				$('.wrapper-content').bind('touchstart', function (e) {
 					ts = e.originalEvent.touches[0].clientY;
@@ -43,4 +45,32 @@ $(function () {
 			},
 		},
 	});
+	//navigation outter change section
+	$('.nav-outter .nav-outter__item').click(function() {
+		let itemMenu = $(this),
+			index = itemMenu.data('index'),
+			wrapper = $('.wrapper'),
+			allItemMenu = $('.nav-outter > .nav-outter__list .nav-outter__item'),
+			hamburger = $('.hamburger');
+
+			sectionsSwiper.slideTo(index);
+			allItemMenu.removeClass('nav-outter__item--active');
+			itemMenu.addClass('nav-outter__item--active');
+			wrapper.removeClass('wrapper--open');
+			hamburger.removeClass('is-active');
+	});
+	//open outter menu
+	$('.hamburger').click(function(){
+		$('.wrapper').toggleClass('wrapper--open');
+		$(this).toggleClass('is-active');
+	});
+	//when section change with scroll
+	sectionsSwiper.on('slideChange', function () {
+		let currentIndex = sectionsSwiper.activeIndex,
+			itemsOutterMenu = $('.nav-outter .nav-outter__item');
+
+			itemsOutterMenu.removeClass('nav-outter__item--active');
+			itemsOutterMenu.parent().find(`[data-index=${currentIndex}]`).addClass('nav-outter__item--active');
+	});
+
 });
