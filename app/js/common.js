7684e71fb221b804bac52abe7b63d9a7911cc863
Vitatiy <targetdeveloper57@gmail.com>
2020-07-18 14:44:10 +0300

    home section done, styling btn

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,7 @@ $(function () {
 	let vh = window.innerHeight * 0.01;
 	// Then we set the value in the --vh custom property to the root of the document
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
-	
+
 	var sectionsSwiper = new Swiper('.swiper-container', {
 		effect: 'fade',
 		autoHeight: true,
@@ -81,8 +81,6 @@ $(function () {
 	});
 	//fixes bug when change orientation, recalculete heigth swiperSlide
 	window.addEventListener('orientationchange', () => {
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
 		setTimeout(() => { sectionsSwiper.update(); }, 300)
 	});
 
